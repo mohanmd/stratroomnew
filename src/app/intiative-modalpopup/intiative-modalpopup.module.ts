d9805d601858f9ee4c87a7023fa3e59351a497c1
mohanmd <mohanmd13@gmail.com>
2020-05-10 19:35:32 +0530

    changes

--- src/app/intiative-modalpopup/intiative-modalpopup.module.ts
+++ src/app/intiative-modalpopup/intiative-modalpopup.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-import { IntiativeModalpopupPage } from './intiative-modalpopup.page';
 
 
 import { IntiativeModalpopupPageRoutingModule } from './intiative-modalpopup-routing.module';
